078d315e566692fb2a641d3a6ba763229e11f5b4
Henry Meier <32826270+hcm444@users.noreply.github.com>
2019-12-11 21:53:28 -0800

    Add files via upload

--- Tic Tac Toe/Tic Tac Toe/ViewController.swift
+++ Tic Tac Toe/Tic Tac Toe/ViewController.swift
@@ -2,42 +2,63 @@
 //  ViewController.swift
 //  Tic Tac Toe
 //
-//  Created by Henry Meier on 12/11/19.
+//  Created by Henry Meier on 12/08/19.
 //  Copyright © 2019 Henry Meier. All rights reserved.
 //
-
 import UIKit
-
 class ViewController: UIViewController {
-
-    var activePlayer = 1
-    var gamestate = [0, 0, 0, 0, 0, 0, 0, 0, 0] //array storing game, every array locatiion a place on board
+    var GameBoard = [0, 0, 0, 0, 0, 0, 0, 0, 0] //array storing game, every array locatiion a place on board
     //array starts at 0, tags start at 1
-    
+    let Combo = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+    var Player = 1
+    var ActiveState = true //check if game is running
     @IBAction func action(_ sender: AnyObject)
     {
-        if (gamestate[sender.tag-1] == 0)
+        if (GameBoard[sender.tag-1] == 0 && ActiveState == true)
         {
-            gamestate[sender.tag-1] = activePlayer //can't override game pieces
-        
-        
-        if (activePlayer == 1)
+            GameBoard[sender.tag-1] = Player //can't override game pieces
+        if (Player == 1)
         {
-            sender.setImage(UIImage(named: "Cross.png"), for: UIControl.State())
-            activePlayer = 2 //swap player
+            sender.setImage(UIImage(named: "X.png"), for: UIControl.State())
+            Player = 2 //swap player
         }
         else
         {
-            sender.setImage(UIImage(named: "Nought.png"), for: UIControl.State())
-            activePlayer = 1 //swap player
+            sender.setImage(UIImage(named: "O.png"), for: UIControl.State())
+            Player = 1 //swap player
         }
     }
+        for combination in Combo
+        {
+            if GameBoard[combination[0]] != 0 && GameBoard[combination[0]] == GameBoard[combination[1]] && GameBoard[combination[1]] == GameBoard[combination[2]]//win conditions
+            {
+                ActiveState = false
+                if GameBoard[combination[0]] == 1
+                {
+                    print ("X HAS WON!")//x wins
+                    //currently outputs to console
+                }
+                else
+                {
+                    print ("O HAS WON!")//o wins
+                    //currently outputs to console
+                }
+            }
+        }
     }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
 }
 
 
