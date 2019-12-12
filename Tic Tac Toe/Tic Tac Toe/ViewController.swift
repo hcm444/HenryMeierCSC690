195c9f72065c7a8f2d95ac699e8a7441bf0f5b6f
Henry Meier <32826270+hcm444@users.noreply.github.com>
2019-12-12 13:01:07 -0800

    Add files via upload
    removed optional label
    
    added a second label
    
    one label for x win condition, one for o win condition

--- Tic Tac Toe/Tic Tac Toe/ViewController.swift
+++ Tic Tac Toe/Tic Tac Toe/ViewController.swift
@@ -19,11 +19,15 @@ class ViewController: UIViewController {
         [0, 4, 8],
         [2, 4, 6]
     ]
+    //win condition array
     var Player = 1
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    
     var ActiveState = true //check if game is running
-    @IBAction func action(_ sender: AnyObject) //AnyObject swift update
+    @IBAction func action(_ sender: AnyObject)
     {
-        if (GameBoard[sender.tag-1] == 0 && ActiveState == true) //check if game is "running"
+        if (GameBoard[sender.tag-1] == 0 && ActiveState == true)
         {
             GameBoard[sender.tag-1] = Player //can't override game pieces
         if (Player == 1)
@@ -37,30 +41,53 @@ class ViewController: UIViewController {
             Player = 1 //swap player
         }
     }
-        for combination in Combo //win condition checker
+        for combination in Combo
         {
-            if GameBoard[combination[0]] != 0 
-            && GameBoard[combination[0]] == GameBoard[combination[1]] 
-            && GameBoard[combination[1]] == GameBoard[combination[2]]//win conditions
+            if GameBoard[combination[0]] != 0
+                && GameBoard[combination[0]] == GameBoard[combination[1]]
+                && GameBoard[combination[1]] == GameBoard[combination[2]]//win conditions
             {
                 ActiveState = false
                 if GameBoard[combination[0]] == 1
                 {
+                    
                     print ("X HAS WON!")//x wins
+                   
+                    label1.text = "X wins!"
+                   //not going to use optionals
+                    
                     //currently outputs to console
                 }
                 else
                 {
-                    print ("O HAS WON!")//o wins
+                   print ("O HAS WON!")//o wins
+                  
+                   label2.text = "O wins!"
+                    //not going to use optionals
+                    
+                   
                     //currently outputs to console
                 }
+                
+                
+                label1.isHidden = false
+                //hide button 1
+                label2.isHidden = false
+                //hide button 2
             }
         }
     }
+   
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        //helps clear unwanted memory 
+    }
+
 }
 
 
